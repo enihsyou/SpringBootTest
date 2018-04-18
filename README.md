ebcc4c4d47f5011ad92ff1f7cfefeeec25e37d19
九条涼果 <s1131234@gmail.com>
2018-04-18 19:37:50 +0800

    更新文档

--- README.md
+++ README.md
@@ -2,3 +2,9 @@
 
 可以选择 `answer` 分支上最新的那个版本，进行对比操作
 ![答案对比方式](img/Snipaste_2018-04-18_18-24-55.png)
+
+[StudentController.java](src/main/java/com/enihsyou/json/server/StudentController.java#L17-L126)
+![](img/Snipaste_2018-04-18_19-35-26.png)
+
+[Student.java](src/main/java/com/enihsyou/json/server/Student.java#L10-L78)
+![](img/Snipaste_2018-04-18_19-35-20.png)
--- src/main/java/com/enihsyou/json/server/Student.java
+++ src/main/java/com/enihsyou/json/server/Student.java
@@ -5,6 +5,7 @@ import org.hibernate.annotations.NaturalId;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import java.util.Objects;
 
 /** 学生表到JavaBean的映射 */
 @Entity
@@ -31,6 +32,46 @@ public class Student {
         this.name = name;
     }
 
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        final Student student = (Student) o;
+        return Objects.equals(id, student.id) &&
+               Objects.equals(number, student.number) &&
+               Objects.equals(name, student.name);
+    }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(id, number, name);
+    }
+
+    public Long getId() {
+
+        return id;
+    }
+
+    public void setId(final Long id) {
+        this.id = id;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(final String number) {
+        this.number = number;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(final String name) {
+        this.name = name;
+    }
     // fixme 一个JavaBean需要getter setter方法，需要实现equals hashCode方法
     // 注意，如果没有 可见性为public的 getter setter方法，json化会失败
     // 尝试使用IntelliJ IDEA的 `Alt+Insert (⌘+N、⌃+↩)` 快捷方法
